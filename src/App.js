4acaea5e290895757b6df0a01d801730bcf79fca
publizm <publizm@gmail.com>
2020-01-06 11:18:10 +0900

    docs: add Todo function complete

--- src/App.js
+++ src/App.js
@@ -14,39 +14,65 @@ export default class App extends Component {
     };
   }
 
+  createId = () => {
+    return Math.max(0, ...this.state.todos.map(todo => todo.id)) + 1;
+  };
+
+  addTodo = ({ key, target, target: { value } }) => {
+    if (key !== 'Enter' || value.trim() === '') return;
+
+    this.setState({
+      todos: [...this.state.todos, { id: this.createId(), content: value, completed: false }],
+    });
+
+    target.value = '';
+  };
+
   render() {
     return (
       <>
-        <div class="container">
-          <h1 class="title">Todos</h1>
-          <div class="ver">2.0</div>
+        <div className="container">
+          <h1 className="title">Todos</h1>
+          <div className="ver">2.0</div>
 
-          <input class="input-todo" placeholder="What needs to be done?" autofocus />
-          <ul class="nav">
-            <li id="all" class="active">
+          <input
+            className="input-todo"
+            placeholder="What needs to be done?"
+            onKeyPress={this.addTodo}
+            autoFocus
+          />
+          <ul className="nav">
+            <li id="all" className="active">
               All
             </li>
             <li id="active">Active</li>
             <li id="completed">Completed</li>
           </ul>
-
-          <ul class="todos">
-            {/* <li id="myId" class="todo-item">
-            <input class="custom-checkbox" type="checkbox" id="ck-myId">
-            <label for="ck-myId">HTML</label>
-            <i class="remove-todo far fa-times-circle"></i>
-            </li> */}
+          <ul className="todos">
+            {this.state.todos &&
+              this.state.todos.map(todo => (
+                <li key={todo.id} id={todo.id} className="todo-item">
+                  <input
+                    className="custom-checkbox"
+                    type="checkbox"
+                    id={'ck-myId' + todo.id}
+                    defaultChecked={todo.completed ? true : false}
+                  />
+                  <label htmlFor={'ck-myId' + todo.id}>{todo.content}</label>
+                  <i className="remove-todo far fa-times-circle"></i>
+                </li>
+              ))}
           </ul>
-          <div class="footer">
-            <div class="complete-all">
-              <input class="custom-checkbox" type="checkbox" id="ck-complete-all" />
-              <label for="ck-complete-all">Mark all as complete</label>
+          <div className="footer">
+            <div className="complete-all">
+              <input className="custom-checkbox" type="checkbox" id="ck-complete-all" />
+              <label htmlFor="ck-complete-all">Mark all as complete</label>
             </div>
-            <div class="clear-completed">
-              <button class="btn">
-                Clear completed (<span class="completed-todos">0</span>)
+            <div className="clear-completed">
+              <button className="btn">
+                Clear completed (<span className="completed-todos">0</span>)
               </button>
-              <strong class="active-todos">0</strong> items left
+              <strong className="active-todos">0</strong> items left
             </div>
           </div>
         </div>
