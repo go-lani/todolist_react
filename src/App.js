60c3192c6e6255bd585cb2572f09c4ba8ed21a0e
publizm <publizm@gmail.com>
2020-01-06 20:24:04 +0900

    docs: Todolist - class component type completed

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default class App extends Component {
         { id: 2, content: 'CSS', completed: true },
         { id: 3, content: 'Javascript', completed: false },
       ],
+      category: 'all',
     };
   }
 
@@ -35,85 +36,97 @@ export default class App extends Component {
   };
 
   toggleCompleted = id => {
-    this.setState(
-      {
-        todos: [
-          ...this.state.todos.map(todo => (todo.id === id ? { ...todo, completed: !todo.completed } : todo)),
-        ],
-      },
-      () => {
-        console.log(this.state.todos);
-      },
-    );
+    this.setState({
+      todos: [...this.state.todos.map(todo => (todo.id === id ? { ...todo, completed: !todo.completed } : todo))],
+    });
   };
 
   allComplete = e => {
-    this.setState(
-      {
-        todos: [...this.state.todos.map(todo => ({ ...todo, completed: e.target.checked }))],
-      },
-      () => {
-        console.log(this.state.todos);
-      },
+    this.setState({
+      todos: [...this.state.todos.map(todo => ({ ...todo, completed: e.target.checked }))],
+    });
+  };
+
+  clearComplete = () => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.completed !== true)],
+    });
+  };
+
+  changeCategory = e => {
+    const Lis = e.target.parentNode.children;
+    const thisLi = e.target;
+
+    this.setState({
+      category: thisLi.id,
+    });
+
+    [...Lis].forEach(li => {
+      li.classList.remove('active');
+    });
+
+    thisLi.classList.add('active');
+  };
+
+  createTodo = todo => {
+    return (
+      <li key={todo.id} id={todo.id} className="todo-item">
+        <input
+          className="custom-checkbox"
+          type="checkbox"
+          id={'ck-myId' + todo.id}
+          checked={todo.completed ? true : false}
+          onChange={() => this.toggleCompleted(todo.id)}
+        />
+        <label htmlFor={'ck-myId' + todo.id}>{todo.content}</label>
+        <i className="remove-todo far fa-times-circle" onClick={() => this.removeTodo(todo.id)}></i>
+      </li>
     );
   };
 
+  renderCategory = (todos, category) => {
+    const _todos = todos;
+    switch (category) {
+      case 'all':
+        return _todos.map(todo => this.createTodo(todo));
+      case 'active':
+        return _todos.filter(todo => todo.completed === true).map(todo => this.createTodo(todo));
+      case 'completed':
+        return _todos.filter(todo => todo.completed === false).map(todo => this.createTodo(todo));
+      default:
+        return _todos.map(todo => this.createTodo(todo));
+    }
+  };
+
   render() {
+    const { todos, category } = this.state;
     return (
       <>
         <div className="container">
           <h1 className="title">Todos</h1>
           <div className="ver">2.0</div>
 
-          <input
-            className="input-todo"
-            placeholder="What needs to be done?"
-            onKeyPress={this.addTodo}
-            autoFocus
-          />
-          <ul className="nav">
+          <input className="input-todo" placeholder="What needs to be done?" onKeyPress={this.addTodo} autoFocus />
+          <ul className="nav" onClick={this.changeCategory}>
             <li id="all" className="active">
               All
             </li>
             <li id="active">Active</li>
             <li id="completed">Completed</li>
           </ul>
-          <ul className="todos">
-            {this.state.todos &&
-              this.state.todos.map(todo => (
-                <li key={todo.id} id={todo.id} className="todo-item">
-                  <input
-                    className="custom-checkbox"
-                    type="checkbox"
-                    id={'ck-myId' + todo.id}
-                    checked={todo.completed ? true : false}
-                    onChange={() => this.toggleCompleted(todo.id)}
-                  />
-                  <label htmlFor={'ck-myId' + todo.id}>{todo.content}</label>
-                  <i className="remove-todo far fa-times-circle" onClick={() => this.removeTodo(todo.id)}></i>
-                </li>
-              ))}
-          </ul>
+          <ul className="todos">{this.renderCategory(todos, category)}</ul>
           <div className="footer">
             <div className="complete-all">
-              <input
-                className="custom-checkbox"
-                type="checkbox"
-                id="ck-complete-all"
-                onChange={this.allComplete}
-              />
+              <input className="custom-checkbox" type="checkbox" id="ck-complete-all" onChange={this.allComplete} />
               <label htmlFor="ck-complete-all">Mark all as complete</label>
             </div>
             <div className="clear-completed">
-              <button className="btn">
+              <button className="btn" onClick={this.clearComplete}>
                 Clear completed (
-                <span className="completed-todos">
-                  {this.state.todos.filter(todo => todo.completed === true).length}
-                </span>
-                )
+                <span className="completed-todos">{todos.filter(todo => todo.completed === true).length}</span>)
               </button>
               <strong className="active-todos">
-                {this.state.todos.filter(todo => todo.completed === true).length}
+                {todos.filter(todo => todo.completed === true).length}
                 items left
               </strong>
             </div>
